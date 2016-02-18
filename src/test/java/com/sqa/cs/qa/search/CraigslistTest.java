0799e57f07b6bb293986728d1b373f66525dda22
Chance Shiflett <chanceshiflett@yahoo.com>
2016-02-17 18:44:00 -0800

    Add data provider for testPost

--- src/test/java/com/sqa/cs/qa/search/CraigslistTest.java
+++ src/test/java/com/sqa/cs/qa/search/CraigslistTest.java
@@ -13,9 +13,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -31,20 +29,11 @@ public class CraigslistTest {
 
 	private StringBuffer verificationErrors = new StringBuffer();
 
-	@AfterTest
-	public void afterTest() {
-		driver.quit();
-		String verificationErrorString = verificationErrors.toString();
-		if (!"".equals(verificationErrorString)) {
-			fail(verificationErrorString);
-		}
-	}
-
-	@BeforeTest
-	public void beforeTest() {
-		driver = new FirefoxDriver();
-		baseUrl = "http://sfbay.craigslist.org/";
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	@DataProvider
+	public Object[][] postData() {
+		return new Object[][] {
+				new Object[] { "Test 1", "testing@test.com", "testing title 1", "94580", "This is only a test" },
+				new Object[] { "Test 2", "testing@test.com", "testing title 2", "94555", "This is only a test" }, };
 	}
 
 	@DataProvider
@@ -69,8 +58,9 @@ public class CraigslistTest {
 		}
 	}
 
-	@Test
-	public void testPost() throws Exception {
+	@Test(dataProvider = "postData", enabled = true)
+	public void testPost(String label, String email, String postTitle, String zipCode, String bodyText)
+			throws Exception {
 		driver.get(baseUrl + "/");
 		driver.findElement(By.cssSelector("#postlks > li > #post")).click();
 		driver.findElement(By.xpath("(//input[@name='id'])[3]")).click();
@@ -78,15 +68,15 @@ public class CraigslistTest {
 		driver.findElement(By.name("n")).click();
 		driver.findElement(By.xpath("(//input[@name='n'])[38]")).click();
 		driver.findElement(By.id("FromEMail")).clear();
-		driver.findElement(By.id("FromEMail")).sendKeys("chanceshiflett@yahoo.com");
+		driver.findElement(By.id("FromEMail")).sendKeys(email);
 		driver.findElement(By.id("ConfirmEMail")).clear();
-		driver.findElement(By.id("ConfirmEMail")).sendKeys("chanceshiflett@yahoo.com");
+		driver.findElement(By.id("ConfirmEMail")).sendKeys(email);
 		driver.findElement(By.id("PostingTitle")).clear();
-		driver.findElement(By.id("PostingTitle")).sendKeys("testing");
+		driver.findElement(By.id("PostingTitle")).sendKeys(postTitle);
 		driver.findElement(By.id("postal_code")).clear();
-		driver.findElement(By.id("postal_code")).sendKeys("94577");
+		driver.findElement(By.id("postal_code")).sendKeys(zipCode);
 		driver.findElement(By.id("PostingBody")).clear();
-		driver.findElement(By.id("PostingBody")).sendKeys("testing craigslist post");
+		driver.findElement(By.id("PostingBody")).sendKeys(bodyText);
 		driver.findElement(By.name("go")).click();
 		driver.findElement(By.cssSelector("section.body > form > button[name=\"go\"]")).click();
 		driver.findElement(By.cssSelector("#publish_bottom > button[name=\"go\"]")).click();
@@ -94,7 +84,7 @@ public class CraigslistTest {
 		Assert.assertEquals("IMPORTANT - FURTHER ACTION IS REQUIRED TO COMPLETE YOUR REQUEST !!!", finished.getText());
 	}
 
-	@Test(dataProvider = "searchData")
+	@Test(dataProvider = "searchData", enabled = false)
 	public void testSearch(String label, String[] keywords) throws Exception {
 		driver.get(baseUrl + "/");
 		driver.findElement(By.cssSelector("a.sof > span.txt")).click();
@@ -104,7 +94,7 @@ public class CraigslistTest {
 		List<WebElement> links = driver.findElements(By.partialLinkText(keywords[0]));
 		System.out.println(label);
 		System.out.println("10 most recent job postings:");
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < links.size(); i++) {
 			System.out
 					.println("(" + (i + 1) + ") " + links.get(i).getText() + " : " + links.get(i).getAttribute("href"));
 		}
